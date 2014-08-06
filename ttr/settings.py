da594d62eefa11999304c7703955e419aee486bb
Luke Sapan <luke@celadonsoftware.com>
2014-08-05 22:23:39 -0400

    Add dev MySQL settings

--- ttr/settings.py
+++ ttr/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'ttr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dustmops_ttpsweb',
+        'USER': 'dustmops_mshsl',
+        'PASSWORD': 'ubercharged',
+        'HOST': '205.185.112.51',
+        'PORT': '3306',
     }
 }
 
