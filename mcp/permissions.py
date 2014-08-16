f1bfeb6d8c6599a6412f59637a20d789b8b36b09
Luke Sapan <luke@celadonsoftware.com>
2014-08-16 13:52:00 -0400

    Add basic shard history API

--- mcp/permissions.py
+++ mcp/permissions.py
@@ -6,9 +6,14 @@
 # Level 400 is an admin
 # Level 500 is a Harv
 permissions = {
+
+    ### 200 ###
     'post_bulletin': 200,
     'approve_name': 200,
     'approve_comment': 200,
+    'view_basic_shard_history': 200,
+
+    ### 400 ###
     'find_user': 400,
     'view_shards': 400,
 }
