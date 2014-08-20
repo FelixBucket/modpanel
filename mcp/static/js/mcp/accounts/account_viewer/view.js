e5af9bb22b7aee4ffa4a80dc3cc1cfb3d4307374
Luke Sapan <luke@celadonsoftware.com>
2014-08-20 02:50:57 -0400

    Add in basic account viewing

--- mcp/static/js/mcp/accounts/account_viewer/view.js
+++ mcp/static/js/mcp/accounts/account_viewer/view.js
@@ -7,12 +7,31 @@ define(['app', 'marionette', 'util', 'text!./template.html', 'server'], function
         },
         render: function(){
             var _this = this;
-            this.$el.html(_.template(template, {STATIC_ROOT: app.STATIC_ROOT}));
+            this.$el.html('<div class="row"><div class="col-md-12"><p><strong><i class="fa fa-spinner fa-spin"></i> Just a sec while I pull that up...</strong></p></div></div>');
 
-            server.get('/api/v1/accounts/' + this.account_id + '/');
+            server.get('/api/v1/accounts/' + this.account_id + '/').done(function(account){
+                _this.renderAccount(account);
+            });
         },
-        onDestroy: function(){
+        renderAccount: function(account){
 
-        }
+            //Add in the species for now
+            var types = {
+                d: 'Dog',
+                c: 'Cat',
+                m: 'Mouse',
+                r: 'Rabbit',
+                f: 'Duck',
+                p: 'Monkey',
+                b: 'Bear',
+                s: 'Pig',
+            };
+
+            _.each(account.toons, function(toon){
+                toon.species = types[toon.headType.substr(0,1)];
+            });
+
+            this.$el.html(_.template(template, {STATIC_ROOT: app.STATIC_ROOT, account: account}));
+        },
     });
 });
