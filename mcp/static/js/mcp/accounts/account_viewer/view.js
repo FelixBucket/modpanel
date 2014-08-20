c6895af7d11b4416010fd5bcb01b243cd2b32fb8
Luke Sapan <luke@celadonsoftware.com>
2014-08-20 19:50:21 -0400

    I'm not toon enough apparently

--- mcp/static/js/mcp/accounts/account_viewer/view.js
+++ mcp/static/js/mcp/accounts/account_viewer/view.js
@@ -27,7 +27,7 @@ define(['app', 'marionette', 'util', 'text!./template.html', 'server'], function
                 s: 'Pig',
             };
             _.each(account.toons, function(toon){
-                toon.species = types[toon.dna.headType.substr(0,1)];
+                if (toon) toon.species = types[toon.dna.headType.substr(0,1)];
             });
 
             this.$el.html(_.template(template, {STATIC_ROOT: app.STATIC_ROOT, account: account}));
